8be5f83b8e58f05e2db780d0db6bb166ae6096ac
Willieee3 <56023197+Willieee3@users.noreply.github.com>
2020-03-05 13:23:18 +0100

    Basic stuff for the project
    Added basics stuff :)

---
+++ MoneyMaker App/Assets/NextStep.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextStep : MonoBehaviour
+{
+    public int currentStep;
+    public string currentSprite = "Stap";
+    public string currentSpriteAndStep;
+    public GameObject stap;
+
+
+    public Vector3 spritePosition = new Vector3();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void NextSprite()
+    {
+        if (currentStep > 0)
+        {
+            stap.SetActive(false);
+        }
+
+        currentStep++;
+
+        currentSpriteAndStep = currentSprite + currentStep;
+
+        stap = GameObject.Find(currentSpriteAndStep);
+
+        stap.SetActive(true);
+
+    }
+}
