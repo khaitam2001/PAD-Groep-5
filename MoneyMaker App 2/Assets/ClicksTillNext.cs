95232f1b9370fef93df340b036b8358a4dab0be7
DDobbel <55737842+DDobbel@users.noreply.github.com>
2020-03-10 14:45:58 +0100

    Moneymaker

---
+++ MoneyMaker App 2/Assets/ClicksTillNext.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ClicksTillNext : MonoBehaviour
+{
+    public int PhotoNumber;
+    public int PhotoID;
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.touchCount > 0)
+        {
+            PhotoNumber++;
+        }
+
+        if (PhotoID == PhotoNumber)
+        {
+            
+        }
+    }
+}
