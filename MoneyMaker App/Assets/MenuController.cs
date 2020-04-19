8af93e1049de0fa6a73c849e02cf26314fddc465
DDobbel <55737842+DDobbel@users.noreply.github.com>
2020-04-19 14:23:10 +0200

    change name bankrun

--- MoneyMaker App/Assets/MenuController.cs
+++ MoneyMaker App/Assets/MenuController.cs
@@ -21,7 +21,7 @@ public class MenuController : MonoBehaviour
     }
     public void SwitchToBankRun()
     {
-        SceneManager.LoadScene("BankruN");
+        SceneManager.LoadScene("Bankrun");
     }
     public void SwitchToPromoFeature()
     {
