439964e96c2951c1399dd2816e33cde153928777
Willieee3 <56023197+Willieee3@users.noreply.github.com>
2020-04-28 13:32:38 +0200

    changed step

--- MoneyMaker App/Assets/ScaleChanger.cs
+++ MoneyMaker App/Assets/ScaleChanger.cs
@@ -7,7 +7,8 @@ public class ScaleChanger : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = new Vector3(959, 539, 0);
+        transform.localScale = new Vector3(318, 212, 3);
     }
 
     // Update is called once per frame
