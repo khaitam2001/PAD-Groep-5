785c2461d7d021e783b446241327eb05e3a013b6
Willieee3 <56023197+Willieee3@users.noreply.github.com>
2020-05-28 12:37:30 +0200

    fixed bug select players

---
+++ MoneyMaker App/Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public int Players;
+
+    public Text mytext = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayerThreeSelected()
+    {
+        Players = 3;
+    }
+}
